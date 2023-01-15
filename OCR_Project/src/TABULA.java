2295bed57627b06a00c3442b3f4e4e1b2ab5a9e9
Sahil <sahilsk565678@gmail.com>
2023-01-15 18:21:06 +0530

    Added Dialogue box

--- OCR_Project/src/TABULA.java
+++ OCR_Project/src/TABULA.java
@@ -6,6 +6,9 @@ import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import org.apache.pdfbox.jbig2.segments.Table;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.poi.ss.usermodel.Cell;
@@ -24,9 +27,9 @@ import technology.tabula.extractors.SpreadsheetExtractionAlgorithm;
 
 public class TABULA {
 	
-	 static void text_to_excel() {
+	 static void text_to_excel(String excel_name) {
 		 String fileName = "D://Img-testing//text.txt";
-		 String excelFileName = "D://Img-testing//excel.xlsx";
+		 String excelFileName = excel_name +".xlsx";
 
 		 // Create a Workbook and a sheet in it
 		 XSSFWorkbook workbook = new XSSFWorkbook();
@@ -60,47 +63,56 @@ public class TABULA {
 		 }
 	}
 	 public static void main(String[] args) throws IOException {
-		    final String FILENAME="D://Img-testing//ROAD DETAILS M WEST.pdf";
-//		    final String FILENAME="D:\\Reconciliation project\\Arif\\Arif__Apr_2022.pdf";
+		 JFileChooser chooser = new JFileChooser();
+		 FileNameExtensionFilter filter = new FileNameExtensionFilter("pdf", "PDF");
+	        chooser.setFileFilter(filter);
+	        int returnVal = chooser.showOpenDialog(null);
+	        if(returnVal == JFileChooser.APPROVE_OPTION) {
+	            String source_file = chooser.getSelectedFile().getAbsolutePath();
+	            final String FILENAME= source_file;
+	            System.out.println(source_file);
+	            PDDocument pd = PDDocument.load(new File(FILENAME));
 
-		    PDDocument pd = PDDocument.load(new File(FILENAME));
+			    int totalPages = pd.getNumberOfPages();
+			    System.out.println("Total Pages in Document: "+totalPages);
 
-		    int totalPages = pd.getNumberOfPages();
-		    System.out.println("Total Pages in Document: "+totalPages);
+			    ObjectExtractor oe = new ObjectExtractor(pd);
+			    SpreadsheetExtractionAlgorithm sea = new SpreadsheetExtractionAlgorithm();
+			    Page page = oe.extract(1);
+			    FileWriter fWriter = new FileWriter(
+	                    "D://Img-testing//text.txt");
+			    // extract text from the table after detecting
+			    List<technology.tabula.Table> table = sea.extract(page);
+			    for(technology.tabula.Table tables: table) {
+			        List<List<RectangularTextContainer>> rows = tables.getRows();
 
-		    ObjectExtractor oe = new ObjectExtractor(pd);
-		    SpreadsheetExtractionAlgorithm sea = new SpreadsheetExtractionAlgorithm();
-		    Page page = oe.extract(1);
-		    FileWriter fWriter = new FileWriter(
-                    "D://Img-testing//text.txt");
-		    // extract text from the table after detecting
-		    List<technology.tabula.Table> table = sea.extract(page);
-		    for(technology.tabula.Table tables: table) {
-		        List<List<RectangularTextContainer>> rows = tables.getRows();
+			        for(int i=0; i<rows.size(); i++) {
 
-		        for(int i=0; i<rows.size(); i++) {
+			            List<RectangularTextContainer> cells = rows.get(i);
 
-		            List<RectangularTextContainer> cells = rows.get(i);
+			            for(int j=0; j<cells.size(); j++) {
+			            	String text = cells.get(j).getText()+"|";
+			  
+			                    fWriter.write(text);
+			         
+			                    // Printing the contents of a file
+			                    System.out.print(text);
+			         
+			                    // Closing the file writing connection
+			                    
+			            }
 
-		            for(int j=0; j<cells.size(); j++) {
-		            	String text = cells.get(j).getText()+"|";
-		  
-		                    fWriter.write(text);
-		         
-		                    // Printing the contents of a file
-		                    System.out.print(text);
-		         
-		                    // Closing the file writing connection
-		                    
-		            }
-
-		            System.out.println();
-		            fWriter.write('\n');
-		        }
-		       
-		    }
-		    fWriter.close();
-		    text_to_excel();
+			            System.out.println();
+			            fWriter.write('\n');
+			        }
+			       
+			    }
+			    fWriter.close();
+			    String[] source_excel_name = source_file.split(".pdf",2);
+	            String excel_name = source_excel_name[0];
+			    text_to_excel(excel_name);
+	        }
+		   
 		    
 		}
 
